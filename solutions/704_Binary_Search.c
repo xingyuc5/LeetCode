5976364fd52a729a43f4c17b2b73288f97675e34
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-11 16:04:20 +0800

    Create C solution

--- solutions/704_Binary_Search.c
+++ solutions/704_Binary_Search.c
@@ -1,3 +1,15 @@
-int search(int *nums, int numsSize, int target)
-{
+int search(int *nums, int numsSize, int target) {
+    int mid, left = 0, right = numsSize - 1;
+
+    while (left <= right) {
+        mid = (right + left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
