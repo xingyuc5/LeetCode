bb530a96186d60a10407c9efd8b994e6ad0fd038
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-11 18:53:32 +0800

    Remove comments

--- solutions/696_Count_Binary_Substrings.c
+++ solutions/696_Count_Binary_Substrings.c
@@ -1,7 +1,6 @@
 int countBinarySubstrings(char *s) {
     int counts = 0, lastConsecutiveCount = 0, currentConsecutiveCount = 0;
     char currentStreak = *s;
-    // Interate through string until nullbyte is reached
     while (*s) {
         if (*s != currentStreak) {
             counts += (lastConsecutiveCount > currentConsecutiveCount)
