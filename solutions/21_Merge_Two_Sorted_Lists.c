9cbe6680289b6ec58ed397fe6e004e0c0dea2b07
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-12 22:56:09 +0800

    Add 21 problem and solution

---
+++ solutions/21_Merge_Two_Sorted_Lists.c
@@ -0,0 +1,9 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {}
