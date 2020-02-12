b04ea709df679fa9cdc3c757993cd754022ea3e3
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-12 22:46:37 +0800

    Optimize using BIT OR

--- solutions/709_To_Lower_Case.c
+++ solutions/709_To_Lower_Case.c
@@ -2,7 +2,7 @@ char* toLowerCase(char* str) {
     int i = 0;
     while (str[i]) {
         if (str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] += 32;
+            str[i] |= 32;
         }
         i++;
     }
