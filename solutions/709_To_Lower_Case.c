f18d32741df4de82d8658c0630dbd5c96b8a8723
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-12 22:45:43 +0800

    Add C solution

--- solutions/709_To_Lower_Case.c
+++ solutions/709_To_Lower_Case.c
@@ -1,1 +1,10 @@
-char* toLowerCase(char* str) { return str; }
+char* toLowerCase(char* str) {
+    int i = 0;
+    while (str[i]) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] += 32;
+        }
+        i++;
+    }
+    return str;
+}
