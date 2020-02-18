e0b5177ba0ab5af8668e5f8f70a51c4fdbb6d121
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-19 00:19:32 +0800

    Add Python Script to Create Markdowns

---
+++ problems/Script.py
@@ -0,0 +1,27 @@
+TEMPLATE_FILE = "template.md"
+QUESTION_FILE = "question.txt"
+
+# Read template string
+template_string = open(TEMPLATE_FILE).read()
+
+# Read question number and names
+lines = open(QUESTION_FILE).readlines()
+
+counter = 0
+file_name = ""
+
+
+for line in lines:
+    line = line.replace("\n", "")
+    if counter % 2 == 0:
+        file_name = line
+        file_name += ". "
+    else:
+        file_name += line
+        text = open(file_name, 'w')
+        text.write("# ")
+        text.write(file_name)
+        text.write("\n")
+        text.write(template_string)
+        text.close()
+    counter += 1
--- problems/template.md
+++ problems/template.md
@@ -1,5 +1,3 @@
-# .
-
 [Link to Question]()
 
 ## Description
