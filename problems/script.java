a236e2a1d8836ab20a2007e4cd62740e6d13e960
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-18 23:47:47 +0800

    Add Comment

--- problems/script.java
+++ problems/script.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
+// Script to create markdown files
 public class Script {
     public static void main(String[] args) {
         // Concate template string
