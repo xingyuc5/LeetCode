5dea86e45e2d1b2d12b7d4bacf0529e498ce30f1
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-19 16:36:45 +0800

    Add script to generate complete message with dates

---
+++ problems/CompleteMessage.py
@@ -0,0 +1,22 @@
+import sys
+import time
+
+q = "%20".join(sys.argv[1:])
+q_num = int(sys.argv[1].rstrip('.'))
+q_name = " ".join(sys.argv[2:])
+final_name = "{:0>4d}. {}".format(q_num, q_name)
+curr_date = time.strftime("%d/%m/%Y", time.localtime())
+
+# By Difficulty
+by_difficulty = "[{}](./problems/{}.md) :white_check_mark: <sub>{}</sub>".format(
+    final_name, q, curr_date)
+
+
+# By Dates
+by_date = "[{}](./problems/{}.md) :white_check_mark:".format(
+    final_name, q)
+
+
+print(by_difficulty)
+print(by_date)
+# print()
