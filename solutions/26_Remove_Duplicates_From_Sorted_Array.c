2e0bd4eab0835f7713a8712361a39a655ed74062
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-22 22:58:36 +0800

    Add comments

--- solutions/26_Remove_Duplicates_From_Sorted_Array.c
+++ solutions/26_Remove_Duplicates_From_Sorted_Array.c
@@ -1,13 +1,20 @@
 int removeDuplicates(int* nums, int numsSize) {
+    // Special case
     if (numsSize == 0) {
         return 0;
     } else if (numsSize == 1) {
         return 1;
     }
 
+    // Normal case
     int len = 1;
+
+    // Iterate through the array, the first number is unique by far
     for (int i = 1; i < numsSize; i++) {
+        // If current number is different from last unique number
         if (nums[i] != nums[len - 1]) {
+            // Increase unique numbers count and update new discovered unique
+            // number
             nums[len] = nums[i];
             len++;
         }
