4221078ce81b3507fc4c7558b1eebab40f6d4975
Xingyu Chen <xingyuc5@student.unimelb.edu.au>
2020-02-26 00:35:52 +0800

    Prettier formmater

--- solutions/35_Search_Insert_Position.py
+++ solutions/35_Search_Insert_Position.py
@@ -3,7 +3,7 @@ class Solution:
         counter = 0
         for i in range(len(nums)):
             if nums[i] < target:
-                counter+=1
+                counter += 1
             else:
                 return counter
         return counter
